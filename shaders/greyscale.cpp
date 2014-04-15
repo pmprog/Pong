4d32ee989f2bb632c5019aad8e7b37c48b9bb3c6
Marq Watkin <devhead@pmprog.co.uk>
2014-04-15 08:00:58 +0100

    Shader perf

--- shaders/greyscale.cpp
+++ shaders/greyscale.cpp
@@ -4,28 +4,26 @@
 
 void ShaderGreyscale::Apply( ALLEGRO_BITMAP* Target )
 {
-	ALLEGRO_BITMAP* o = FRAMEWORK->Display_GetCurrentTarget();
+	PackedARGB8888* gsCol;
 
 	int imgW = al_get_bitmap_width( Target );
 	int imgH = al_get_bitmap_height( Target );
 
 	ALLEGRO_LOCKED_REGION* rgn = al_lock_bitmap( Target, ALLEGRO_PIXEL_FORMAT_ABGR_8888_LE, ALLEGRO_LOCK_READWRITE );
 
-	FRAMEWORK->Display_SetTarget( Target );
-
 	for( int y = 0; y < imgH; y++ )
 	{
 		for( int x = 0; x < imgW; x++ )
 		{
-			ALLEGRO_COLOR c = al_get_pixel( Target, x, y );
-			c.r = (c.r * 0.35f) + (c.g * 0.5f) + (c.b * 0.15f);
-			c.g = c.r;
-			c.b = c.r;
-			al_put_pixel( x, y, c );
+			//gsCol = &((PackedARGB8888*)rgn->data)[(y * rgn->pitch) + x];
+			PackedARGB8888* pxRow = (PackedARGB8888*)((char*)rgn->data + (y * rgn->pitch));
+			gsCol = &pxRow[x];
+			gsCol->r = (unsigned char)(((float)gsCol->r * 0.35) + ((float)gsCol->g * 0.5) + ((float)gsCol->b * 0.15));
+			gsCol->g = gsCol->r;
+			gsCol->b = gsCol->r;
 		}
 	}
 
 	al_unlock_bitmap( Target );
 
-	FRAMEWORK->Display_SetTarget( o );
 }
--- shaders/scanlines.cpp
+++ shaders/scanlines.cpp
@@ -18,16 +18,12 @@ ShaderScanlines::ShaderScanlines(int NonScanWidth, int ScanWidth, int ScanDecrea
 
 void ShaderScanlines::Apply( ALLEGRO_BITMAP* Target )
 {
-	ALLEGRO_BITMAP* o = FRAMEWORK->Display_GetCurrentTarget();
-
+	PackedARGB8888* gsCol;
 	int imgW = al_get_bitmap_width( Target );
 	int imgH = al_get_bitmap_height( Target );
 	int linesForScan = nonScanLineWidth + scanLineWidth;
-	float decCol = scanDecrease / 255.0f;
 
 	ALLEGRO_LOCKED_REGION* rgn = al_lock_bitmap( Target, ALLEGRO_PIXEL_FORMAT_ABGR_8888_LE, ALLEGRO_LOCK_READWRITE );
-
-	FRAMEWORK->Display_SetTarget( Target );
 
 	for( int y = 0; y < imgH; y++ )
 	{
@@ -35,15 +31,15 @@ void ShaderScanlines::Apply( ALLEGRO_BITMAP* Target )
 		{
 			for( int x = 0; x < imgW; x++ )
 			{
-				ALLEGRO_COLOR c = al_get_pixel( Target, x, y );
-				c.r = (c.r > decCol ? c.r - decCol : 0.0f);
-				c.g = (c.g > decCol ? c.g - decCol : 0.0f);
-				c.b = (c.b > decCol ? c.b - decCol : 0.0f);
-				al_put_pixel( x, y, c );
+				PackedARGB8888* pxRow = (PackedARGB8888*)((char*)rgn->data + (y * rgn->pitch));
+				gsCol = &pxRow[x];
+				gsCol->r = (gsCol->r > scanDecrease ? gsCol->r - scanDecrease : 0);
+				gsCol->g = (gsCol->g > scanDecrease ? gsCol->g - scanDecrease : 0);
+				gsCol->b = (gsCol->b > scanDecrease ? gsCol->b - scanDecrease : 0);
 			}
 		}
 	}
 
 	al_unlock_bitmap( Target );
-	FRAMEWORK->Display_SetTarget( o );
+
 }
